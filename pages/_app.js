257132046c05ba40b326ed923f6b37a8b1b7eb2b
Iskandarov Nodirbek <iskandarovnodir@Nodirbeks-MacBook-Air.local>
2023-05-25 13:58:41 +0500

    commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,17 @@
 // import 'bootstrap/dist/css/bootstrap.css'
-import Layout from '@/components/Layout'
-import '@/styles/globals.css'
+// import Layout from '@/components/Layout'
+// import '@/styles/globals.css'
+
+import { LayoutProvider } from '../layouta/context/layoutcontext';
+import Layout from '../layouta/layout';
+
+import 'primereact/resources/primereact.css';
+import 'primeflex/primeflex.css';
+import 'primeicons/primeicons.css';
+
+import '../styles/layout/layout.scss';
+// import '../styles/demo/Demos.scss';
+
 import '../styles/test.css'
 import '../styles/elektromobil.css'
 import '../styles/korporativ.css'
@@ -16,10 +27,16 @@ import { appWithTranslation } from 'next-i18next';
 // import { i18n } from '../next-i18next.config';
 
 function App({ Component, pageProps }) {
-  return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+  if (Component.getLayout) {
+    return <LayoutProvider>{Component.getLayout(<Component {...pageProps} />)}</LayoutProvider>;
+} else {
+    return (
+        <LayoutProvider>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </LayoutProvider>
+    );
+}
 }
 export default appWithTranslation(App)
---
+++ pages/layout.js
@@ -0,0 +1,11 @@
+import Loyihalar from "@/components/loyihalar/Loyihalar"
+import Layout from "@/src/components/layout"
+
+function layout () {
+    return (
+        <div>
+            <Layout/>
+        </div>
+    )
+}
+export default layout
